2dd87567c6363b2f9da16f35c32fbf2d727d7f03
Aman23Gupta <amang234284@gmail.com>
2021-08-03 15:20:20 +0530

    basic setup for django

---
+++ api/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def main(request):
+    return HttpResponse("<h1>Hello</h1>")
+
+# Create your views here.
