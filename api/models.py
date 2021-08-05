06ddd714a9f9f7dc29053099b97678d2063861f5
Aman23Gupta <amang234284@gmail.com>
2021-08-05 14:53:39 +0530

    django model to create room

--- api/models.py
+++ api/models.py
@@ -2,25 +2,25 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.db.models.fields import AutoField
 import string
 import random
 
+
 def generate_unique_code():
     length = 6
-    
+
     while True:
-        code = ''.join(random.choices(string.ascii_uppercase, k = length))
-        if Room.objects.filter(code = code).count() == 0:
+        code = ''.join((random.choice(string.ascii_uppercase) for x in range(length)))
+        if Room.objects.filter(code=code).count() == 0:
             break
-    
+
     return code
 
 
-# Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length = 8, default = "", unique = True)
-    host = models.CharField(max_length = 50, unique = True)
-    guest_can_pause = models.BooleanField(null = False, default = False)
-    votes_to_skip = models.IntegerField(null = False, default = 1)
-    created_at = models.DateTimeField(auto_now_add = True)
+    code = models.CharField(
+        max_length=8, default=generate_unique_code, unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import RoomView
+from .views import RoomView,CreateRoomView
 
 urlpatterns = [
-    url(r'^room/',RoomView.as_view())
+    url(r'^room/',RoomView.as_view()),
+    url(r'^create-room',CreateRoomView.as_view())
 ]
