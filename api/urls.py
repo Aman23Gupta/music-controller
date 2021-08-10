71238f2d43fc1029d48ad16de2b8938bfae2405c
Aman23Gupta <amang234284@gmail.com>
2021-08-10 22:11:07 +0530

    Front/back - end for join room

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import GetRoom, RoomView,CreateRoomView
+from .views import GetRoom, RoomView,CreateRoomView,JoinRoom
 
 urlpatterns = [
     url(r'^room/',RoomView.as_view()),
     url(r'^create-room',CreateRoomView.as_view()),
-    url(r'^get-room',GetRoom.as_view())
+    url(r'^get-room',GetRoom.as_view()),
+    url(r'^join-room', JoinRoom.as_view())
 ]
