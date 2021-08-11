d3b0e67554879fe047060a83b20bebbbe97f79e4
Aman23Gupta <amang234284@gmail.com>
2021-08-11 22:15:00 +0530

    homepage front/backend

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import GetRoom, RoomView,CreateRoomView,JoinRoom
+from .views import GetRoom, RoomView,CreateRoomView,JoinRoom,UserInRoom
 
 urlpatterns = [
     url(r'^room/',RoomView.as_view()),
     url(r'^create-room',CreateRoomView.as_view()),
     url(r'^get-room',GetRoom.as_view()),
-    url(r'^join-room', JoinRoom.as_view())
+    url(r'^join-room', JoinRoom.as_view()),
+    url(r'^user-in-room', UserInRoom.as_view())
 ]
