7716a62532cb87353384c2f6ae9b65f2b557bbdd
Aman23Gupta <amang234284@gmail.com>
2021-08-12 14:08:28 +0530

    deleting the session

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from .views import GetRoom, RoomView,CreateRoomView,JoinRoom,UserInRoom
+from .views import GetRoom, RoomView,CreateRoomView,JoinRoom,UserInRoom,LeaveRoom
 
 urlpatterns = [
     url(r'^room/',RoomView.as_view()),
     url(r'^create-room',CreateRoomView.as_view()),
     url(r'^get-room',GetRoom.as_view()),
     url(r'^join-room', JoinRoom.as_view()),
-    url(r'^user-in-room', UserInRoom.as_view())
+    url(r'^user-in-room', UserInRoom.as_view()),
+    url(r'^leave-room', LeaveRoom.as_view())
 ]
