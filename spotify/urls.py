137954371cb7a36da9cde311eeca476c635853e6
Aman23Gupta <amang234284@gmail.com>
2021-08-23 15:14:11 +0530

    spotify 2

--- spotify/urls.py
+++ spotify/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import AuthURL, spotify_callback, IsAuthenticated
+from .views import *
 
 urlpatterns = [
     url(r'^get-auth-url', AuthURL),
     url(r'^redirect', spotify_callback),
-    url(r'^is-authenticated', IsAuthenticated.as_view())
+    url(r'^is-authenticated', IsAuthenticated.as_view()),
+    url(r'^current-song', CurrentSong.as_view())
 ]
